9f36f83bf38e89daa1d8036b507e221b796c665d
isaacn@myhcit.com <isaacn@myhcit.com>
2019-05-24 00:16:50 -0400

    initial commit

---
+++ EFCoreDisconnectedEntitiesTest/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Castle.Core.Logging;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+namespace EFCoreDisconnectedEntitiesTest
+{
+    class Program
+    {
+
+
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            // In - memory database only exists while the connection is open
+         
+
+                //load from database
+             
+             
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Ethnicity> Ethnicities { get; set; }
+       public static readonly Microsoft.Extensions.Logging.ILoggerFactory consoleLoggerFactory
+         = new LoggerFactory().AddConsole();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLoggerFactory(consoleLoggerFactory);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ethnicity>().HasData(new[]
+            {
+                new Ethnicity(){Description = "Caucasian", EthnicityId = 1},
+                new Ethnicity() {EthnicityId = 2, Description = "Hispanic"}
+            });
+        }
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [ForeignKey("EthnicityId")]
+        public virtual Ethnicity Ethnicity { get; set; }
+        public int EthnicityId { get; set; }
+    }
+
+    public class Ethnicity
+    {
+        public int EthnicityId { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,132 @@
+
+using System.Linq;
+using EFCoreDisconnectedEntitiesTest;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+
+        private DataContext sut;
+        [TestInitialize]
+        public void TestSetup()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseSqlite(connection, e => e.SuppressForeignKeyEnforcement())
+                .Options;
+            sut = new DataContext(options);
+
+            sut.Database.EnsureDeleted();
+            sut.Database.EnsureCreated();
+            var employee = new Employee()
+            {
+                FirstName = "Test",
+                LastName = "Person",
+                EthnicityId = 1
+
+            };
+
+            sut.Employees.Add(employee);
+            sut.SaveChanges();
+        }
+
+
+        [TestMethod]
+        public void When_LoadingEntity_AsNoTracking_Updating_FK_SavesValueToDatabase()
+        {
+            var empFromDB = sut.Employees.Include(e => e.Ethnicity).AsNoTracking()
+                 .FirstOrDefault(e => e.EmployeeId == 1);
+
+            empFromDB.EthnicityId = 2;//Change the foreign key
+
+
+            var keyProperty = sut.Model.FindEntityType(typeof(Employee)).FindPrimaryKey().Properties[0];
+            var locallyTrackedEntity = sut.ChangeTracker.Entries<Employee>().FirstOrDefault(e => e.Property(keyProperty.Name).CurrentValue.ToString() == GetPropValue(empFromDB, keyProperty.Name).ToString());
+            if (locallyTrackedEntity != null)
+            {
+                sut.Entry(locallyTrackedEntity.Entity).State = EntityState.Detached;
+            }
+             
+            sut.Employees.Update(empFromDB);
+            sut.SaveChanges();
+
+            //load from database
+            var empFromDB1 = sut.Employees.Include(e => e.Ethnicity)
+                .FirstOrDefault(e => e.EmployeeId == 1);
+
+            Assert.AreEqual(2, empFromDB1.EthnicityId);
+
+        }
+
+
+        [TestMethod]
+        public void When_LoadingEntity_AsNoTracking_and_Missing_Includes_Updating_FK_SavesValueToDatabase()
+        {
+            var empFromDB = sut.Employees.AsNoTracking()
+                 .FirstOrDefault(e => e.EmployeeId == 1);
+
+            empFromDB.EthnicityId = 2;//Change the foreign key
+
+
+            var keyProperty = sut.Model.FindEntityType(typeof(Employee)).FindPrimaryKey().Properties[0];
+            var locallyTrackedEntity = sut.ChangeTracker.Entries<Employee>().FirstOrDefault(e => e.Property(keyProperty.Name).CurrentValue.ToString() == GetPropValue(empFromDB, keyProperty.Name).ToString());
+            if (locallyTrackedEntity != null)
+            {
+                sut.Entry(locallyTrackedEntity.Entity).State = EntityState.Detached;
+            }
+
+            sut.Employees.Update(empFromDB);
+            sut.SaveChanges();
+
+            //load from database
+            var empFromDB1 = sut.Employees.Include(e => e.Ethnicity)
+                .FirstOrDefault(e => e.EmployeeId == 1);
+
+            Assert.AreEqual(2, empFromDB1.EthnicityId);
+
+        }
+
+
+
+
+        [TestMethod]
+        public void When_LoadingEntity_WITHOUT_AsNoTracking_Updating_FK_SavesValueToDatabase()
+        {
+            var empFromDB = sut.Employees.Include(e => e.Ethnicity)
+                 .FirstOrDefault(e => e.EmployeeId == 1);
+
+            empFromDB.EthnicityId = 2;//Change the foreign key
+
+
+            var keyProperty = sut.Model.FindEntityType(typeof(Employee)).FindPrimaryKey().Properties[0];
+            var locallyTrackedEntity = sut.ChangeTracker.Entries<Employee>().FirstOrDefault(e => e.Property(keyProperty.Name).CurrentValue.ToString() == GetPropValue(empFromDB, keyProperty.Name).ToString());
+            if (locallyTrackedEntity != null)
+            {
+                sut.Entry(locallyTrackedEntity.Entity).State = EntityState.Detached;
+            }
+
+            sut.Employees.Update(empFromDB);
+            sut.SaveChanges();
+
+            //load from database
+            var empFromDB1 = sut.Employees.Include(e => e.Ethnicity)
+                .FirstOrDefault(e => e.EmployeeId == 1);
+
+            Assert.AreEqual(2, empFromDB1.EthnicityId);
+
+        }
+         
+    }
+}
